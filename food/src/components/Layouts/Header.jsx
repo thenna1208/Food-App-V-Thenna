c91a602555e0cb210e32a7fdfd3eee7f06d854e4
thenna1208 <thennavan.m@gmail.com>
2025-04-01 21:22:02 +0530

    Section5,6,7

--- food/src/components/Layouts/Header.jsx
+++ food/src/components/Layouts/Header.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import Logo from "../../assets/logo/logo.png";
 import "../../styles/HeaderStyle.css";
 
 const Header = () => {
   const [nav, setNav] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   // Scroll Navbar
   const changeValueOnScroll = () => {
@@ -17,6 +19,25 @@ const Header = () => {
     window.addEventListener("scroll", changeValueOnScroll);
     return () => window.removeEventListener("scroll", changeValueOnScroll);
   }, []);
+
+  const handleNavClick = (path, sectionId) => {
+    if (location.pathname === path) {
+      // If we're already on the target page, just scroll to section
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    } else {
+      // Navigate to the page then scroll to section
+      navigate(path);
+      setTimeout(() => {
+        const element = document.getElementById(sectionId);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 100);
+    }
+  };
 
   return (
     <header>
@@ -34,27 +55,51 @@ const Header = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/">
+              <Nav.Link 
+                as={Link} 
+                to="/" 
+                onClick={() => handleNavClick("/", "home-section")}
+              >
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/about">
+              <Nav.Link 
+                as={Link} 
+                to="/" 
+                onClick={() => handleNavClick("/", "about-section")}
+              >
                 About
               </Nav.Link>
-              <Nav.Link as={Link} to="/menu">
+              <Nav.Link 
+                as={Link} 
+                to="/" 
+                onClick={() => handleNavClick("/", "menu-section")}
+              >
                 Our Menu
               </Nav.Link>
-              <Nav.Link as={Link} to="/shop">
+              <Nav.Link 
+                as={Link} 
+                to="/" 
+                onClick={() => handleNavClick("/", "shop-section")}
+              >
                 Shop
               </Nav.Link>
-              <Nav.Link as={Link} to="/blog">
+              <Nav.Link 
+                as={Link} 
+                to="/" 
+                onClick={() => handleNavClick("/", "blog-section")}
+              >
                 Blog
               </Nav.Link>
-              <Nav.Link as={Link} to="/contact">
+              <Nav.Link 
+                as={Link} 
+                to="/" 
+                onClick={() => handleNavClick("/", "contact-section")}
+              >
                 Contact
               </Nav.Link>
               <Nav.Link as={Link} to="/">
                 <div className="cart">
-                  <i class="bi bi-bag fs-5"></i>
+                  <i className="bi bi-bag fs-5"></i>
                   <em className="roundpoint">2</em>
                 </div>
               </Nav.Link>
--- food/src/pages/Home/Home.jsx
+++ food/src/pages/Home/Home.jsx
@@ -4,15 +4,48 @@ import Section1 from './Section1'
 import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
+import Section5 from './Section5'
+import Section6 from './Section6'
+import Section7 from './Section7'
 
 function Home() {
   return (
     <div>
       <Layout>
-       <Section1/>
-       <Section2/>
-       <Section3/>
-       <Section4/>
+        {/* Home Section Hero Banner */}
+        <section id="home-section">
+          <Section1 />
+        </section>
+
+        {/* Home Section About */}
+        <section id="about-section">
+          <Section2 />
+        </section>
+
+        {/* Home Section Menu */}
+        <section id="menu-section">
+          <Section3 />
+        </section>
+
+        {/* Home Section Promotion */}
+        <section id="promotion-section">
+          <Section4 />
+        </section>
+
+        {/* Home Section Shop */}
+        <section id="shop-section">
+          <Section5 />
+        </section>
+
+        {/* Home Section Blog */}
+        <section id="blog-section">
+          <Section6 />
+        </section>
+
+        {/* Home Section Contact */}
+        <section id="contact-section">
+          <Section7 />
+        </section>
       </Layout>
     </div>
   )
